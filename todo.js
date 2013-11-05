39b1870f9f195c8dc0c2e87d671381da0609bbde
lkozloff <lyle@asianhope.org>
2013-11-05 15:35:25 +0700

    first working version

--- todo.js
+++ todo.js
@@ -1,13 +1,25 @@
 function TodoCtrl($scope){
-    $scope.todos = [
-    {text: 'hello', done: true},
-    {text: 'goodbye', done: true},
-    {text: 'bonjour', done: true},
-   ];
+   $scope.todos = JSON.parse(localStorage.getItem("todos") || '[]');
 
    $scope.addItem = function() {
     $scope.todos.push({text: $scope.item, done: false});
     $scope.item = "";
    };
 
+    $scope.clearCompleted = function(){
+        var old = $scope.todos;
+        $scope.todos=[];
+        angular.forEach(old, function(task){
+            if(!task.done) $scope.todos.push(task);
+        });
+    };
+
+    $scope.remove = function(todo){
+        var index = $scope.todos.indexOf(todo);
+        $scope.todos.splice(index,1);
+    };
+
+    $scope.save = function(){
+        localStorage.setItem("todos",JSON.stringify($scope.todos));
+    };
 }
